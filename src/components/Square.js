e84a1e96380e44e976e29f9199238dfe16f1aa2f
chomeko3o <kusyopi@gmail.com>
2020-02-15 13:56:45 +0900

    自分のstateをもたいない関数コンポーネント化

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,18 +1,16 @@
 import React from 'react';
 //boardによって制御されたコンポーネントになった
-class Square extends React.Component {
-  render() {
-    return (
-      //クリックしたら親の(board)props.onClickイベントで関数を呼び出す
-      //stateの値が親から受け取ったvalueの値になる
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    //クリックしたら親の(board)props.onClickイベントで関数を呼び出す
+    //stateの値が親から受け取ったvalueの値になる
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
