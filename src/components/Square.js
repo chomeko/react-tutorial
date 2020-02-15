b64371762cc4c554426881ba09128b223b806ffb
chomeko3o <kusyopi@gmail.com>
2020-02-15 12:52:24 +0900

    squareをクリックすると現在のステータスがXになる

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class Square extends React.Component {
+  //this.stateで初期値を保存する
+  //propsは親の事、今回の親はBoardになってる
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      //画面上にBoardから受け取ったpropsのvalueを表示させる
-      <button className="square">
-        {this.props.value}
+      //クリックしたらthis.setStateでvalueの値をXに書き換える
+      //stateの値が初期値からXになる
+      <button
+        className="square"
+        onClick={() => this.setState({ value: 'X' })}
+      >
+        {this.state.value}
       </button>
     );
   }
